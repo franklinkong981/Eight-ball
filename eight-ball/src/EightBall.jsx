a9c50b7147f0b1373f0573272e75ed86f4b0c17b
Franklin Kong <franklinkong981@gmail.com>
2024-12-17 14:34:25 -0800

    Changed initial msg and color to useState hook

--- eight-ball/src/EightBall.jsx
+++ eight-ball/src/EightBall.jsx
@@ -3,10 +3,14 @@ import React, {useState} from "react";
 import "./EightBall.css";
 
 const EightBall = ({answers}) => {
+  const [color, setColor] = useState("black");
+  const [message, setMessage] = useState("Think of a question");
+
+  
   return (
     <>
-      <div class="EightBall" style={{color: "white", backgroundColor: "black"}}>
-        <p class="EightBall-message">Think of a question</p>
+      <div class="EightBall" style={{backgroundColor: color}}>
+        <p class="EightBall-message">{message}</p>
       </div>
     </>
   );
